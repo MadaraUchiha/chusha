092931766b842f4be71c27ab467f3549e1acf901
Dor Tchizik <dor.t@taboola.com>
2015-01-29 23:01:44 +0200

    Add UMD dist file building

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,6 +2,10 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 var mocha = require('gulp-mocha');
+var sixToFive = require('gulp-6to5');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var del = require('del');
 
 gulp.task('lint', function() {
     gulp.src(['src/**/*.js', 'test/**/*.js'])
@@ -20,4 +24,21 @@ gulp.task('test', function() {
         }));
 });
 
-gulp.task('default', ['lint', 'test']);
+gulp.task('clean', function() {
+    del('dist');
+});
+
+gulp.task('dist', ['lint', 'test', 'clean'], function() {
+    gulp.src(['src/chusha.js'])
+        .pipe(sixToFive({
+            // Note: I'm not sure what the difference between umd and umdStrict is.
+            // I like being stricter so I'll go with umdStrict for now.
+            modules: 'umd'
+        }))
+        .pipe(gulp.dest('dist'))
+        .pipe(uglify())
+        .pipe(rename('chusha.min.js'))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['lint', 'test', 'dist']);
